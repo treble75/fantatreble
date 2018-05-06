0d159e30dc399964ca69c73316409578f781ae13
Luca Guerrieri <guerrieriluca@gmail.com>
2018-05-06 19:17:22 +0200

    Fix calciomercato e aggiunta pagina trattative in corso

---
+++ application/views/utenti/trattative.php
@@ -0,0 +1,79 @@
+
+        <!-- Page Heading
+        ================================================== -->
+        <div class="page-heading">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                        <h1 class="page-heading__title">Trattative <span class="highlight">Calciomercato</span></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Content
+        ================================================== -->
+        <div class="site-content">
+            <div class="container">
+
+
+                <!-- Team Roster: Table -->
+                <div class="card card--has-table">
+                    <div class="card__header">
+                        <h4>Trattative Calciomercato</h4>
+                        <span style="font-size: 12px;">Sono visibili soltanto le offerte ancora aperte e non ancora scadute</span>
+                    </div>
+                    <div class="card__content">
+                        <div class="table-responsive">
+                            <table class="table table--lg team-roster-table">
+                                <thead>
+                                    <tr>
+                                        <th class="team-roster-table__number" style="width: 15%">&nbsp;</th>
+                                        <th class="team-roster-table__name" style="width: 15%; text-align: center;">Squadra</th>
+                                        <th class="team-roster-table__age" style="width: 50%; text-align: center;">Offerta</th>
+                                        <th class="team-roster-table__name" style="width: 20%; text-align: center;">Scadenza</th>
+                                    </tr>
+                                </thead>
+                                <?php
+                                if ($offerte) {
+                                    $color = "#CCFFCC";
+                                    ?>
+                                    <tbody>
+                                        <?php
+                                        foreach ($offerte as $row) { 
+                                            ?>
+
+                                            <tr style="padding: 0px 0px;" >
+                                                
+                                                <td class="team-roster-table__number" style="padding: 0px 0px; vertical-align: middle; width: 15%;" align="center">
+                                                    <figure class="team-leader__player-img team-leader__player-img--sm">
+                                                        <img src="<?= base_url('/') ?>images/users/<?= $row['id_utente'] ?>.png" />
+                                                    </figure>
+                                                </td>
+                                                <td class="team-roster-table__name" style="color: #1892ED; font-size: 13px; vertical-align: middle; width: 15%; text-align: center;"><?= $this->mdl_utenti->getSquadra($row['id_utente']) ?></td>
+                                                <td class="team-roster-table__age" style="color: #1892ED; font-size: 13px; vertical-align: middle; width: 40%; text-align: center;"><?= $row['offerta'] ?></td>
+                                                <?php
+                                                $ora_offerta = substr($row['orario'], 19, 4) . "-" . substr($row['orario'], 16, 2) . "-" . (substr($row['orario'], 13, 2) + 02) . " " . substr($row['orario'], 0, 8);
+                                                ?>
+                                                <td class="team-roster-table__name" style="vertical-align: middle; width: 20%; text-align: center;"><div class="countdown-counter" style="width: 200px;" data-date="<?= $ora_offerta ?>" ></div></td>
+                                            </tr>
+                                            <?php
+                                        }
+                                        ?>
+                                    </tbody>
+                                    <?php
+                                } else
+                                    echo "<h6>Nessuna partita ancora disputata</h6>"
+                                    ?>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- Team Roster: Table / End -->
+
+            </div>
+        </div>
+
+        <!-- Content / End -->
+
