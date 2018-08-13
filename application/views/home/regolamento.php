c15d436ce255f8d7b97004fa538850aae6eaf910
Luca Guerrieri <guerrieriluca@gmail.com>
2018-08-13 10:08:59 +0200

    Aggiunti sfondi

--- application/views/home/regolamento.php
+++ application/views/home/regolamento.php
@@ -2,7 +2,7 @@
 
         <!-- Page Heading
         ================================================== -->
-        <div class="page-heading">
+        <div class="page-heading page-heading-regolamento">
             <div class="container">
                 <div class="row">
                     <div class="col-md-10 col-md-offset-1">
--- application/views/home/risultati_champions_precedenti.php
+++ application/views/home/risultati_champions_precedenti.php
@@ -24,7 +24,7 @@
 
         <!-- Page Heading
         ================================================== -->
-        <div class="page-heading">
+        <div class="page-heading page-heading-risultati-champions-precedenti">
             <div class="container">
                 <div class="row">
                     <div class="col-md-10 col-md-offset-1">
